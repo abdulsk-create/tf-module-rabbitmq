f8de0346f16fcd369c501aad5131ae8152e3dd7c
abdul <abdulsk054@gmail.com>
2023-09-01 10:38:22 +0530

    terraform

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yum install ansible -yum &>>/opt/userdata.log
-ansible-playbook -i localhost, -U https://github.com/abdulsk-create/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
+yum install ansible -y &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/abdulsk-create/roboshop-ansible.git main.yml -e component=rabbitmq &>>/opt/userdata.log
